7c217b4bc88ac1c95d5771aa040dc8ac31927bff
ddlam <ddlam@hotmail.com>
2020-02-16 23:11:13 +0800

    New FIle
    New File

---
+++ TreeStructure/TreeStructure/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TreeStructure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(109, 36);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(693, 419);
+            this.treeView1.TabIndex = 0;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(3, 7);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(785, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "D:\\Documents\\Adobe\\Premiere Pro\\6.0";
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(3, 62);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 2;
+            this.btnView.Text = "btnView";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.BtnView_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(3, 105);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "btnCopy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(3, 36);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(100, 20);
+            this.txtDay.TabIndex = 4;
+            this.txtDay.Text = "5000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "TreeStructureForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtDay;
+    }
+}
+
---
+++ TreeStructure/TreeStructure/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeStructure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            try {
+                foreach (var directory in directoryInfo.GetDirectories())
+                    directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+                foreach (var file in directoryInfo.GetFiles()
+                    .Where(name => !name.FullName.EndsWith(".av", StringComparison.OrdinalIgnoreCase))
+                    .Where(name => !name.FullName.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
+                    )
+                {
+                    if (file.LastWriteTime > DateTime.Now.AddDays(Convert.ToInt32(txtDay.Text) * -1))
+                    {
+                        directoryNode.Nodes.Add(new TreeNode(file.Name));
+                    }
+                }
+            } 
+            catch (AggregateException ex)
+            {
+                MessageBox.Show(ex.StackTrace.ToString());
+            }
+
+
+            return directoryNode;
+        }
+        //
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes.Count > 0)
+            {
+                if (e.Node.Nodes[0].Text == "..." && e.Node.Nodes[0].Tag == null)
+                {
+                    e.Node.Nodes.Clear();
+
+
+                    string[] dirs = Directory.GetDirectories(e.Node.Tag.ToString());
+
+                    foreach (string dir in dirs)
+                    {
+                        DirectoryInfo di = new DirectoryInfo(dir);
+                        TreeNode node = new TreeNode(di.Name, 0, 1) { Checked = true };
+
+                        try
+                        {
+                            node.Tag = dir;
+
+
+                            if (di.GetDirectories().Count() > 0)
+                                node.Nodes.Add(null, "...", 0, 0).Checked = node.Checked;
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            node.ImageIndex = 12;
+                            node.SelectedImageIndex = 12;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Tree_to_Grid", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            node.Checked = e.Node.Checked;
+                            e.Node.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            btnView.Enabled = false;
+            TreeNode node = e.Node;
+            bool is_checked = node.Checked;
+            foreach (TreeNode childNode in e.Node.Nodes)
+            {
+                childNode.Checked = e.Node.Checked;
+            }
+            treeView1.SelectedNode = node;
+        }
+
+        public void CheckAllNodes(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                node.Checked = true;
+                CheckChildren(node, true);
+            }
+        }
+
+        public void UncheckAllNodes(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                node.Checked = false;
+                CheckChildren(node, false);
+            }
+        }
+
+        private void CheckChildren(TreeNode rootNode, bool isChecked)
+        {
+            foreach (TreeNode node in rootNode.Nodes)
+            {
+                CheckChildren(node, isChecked);
+                node.Checked = isChecked;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DirectoryCopy(txtPath.Text, "C:\\AcquireUpdate", true);
+        }
+        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+
+
+                if (file.LastWriteTime > DateTime.Now.AddDays(Convert.ToInt32(txtDay.Text) * -1)
+                        && file.FullName.EndsWith(".av", StringComparison.OrdinalIgnoreCase)
+                        && file.FullName.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase)
+
+                    )
+                {
+
+                    file.CopyTo(temppath, false);
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        private void BtnView_Click(object sender, EventArgs e)
+        {
+
+            ListDirectory(treeView1,txtPath.Text);
+            CheckAllNodes(treeView1.Nodes);
+        }
+    }
+}
